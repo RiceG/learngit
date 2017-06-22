962177df2c226a6aa5ec7a5de4fff3d1ae15e232
wg <591865888@qq.com>
2017-06-23 00:28:07 +0800

    hello

---
+++ web/lib/custom/tags.js
@@ -0,0 +1,68 @@
+(function (Vue, window) {
+  var template = `
+  <div class="tags-wrap" @paste="pasteText($event)">
+    <div class="tags" transition="tags" :style="{backgroundColor: bgc[item.bgc_no]}" v-for="(item, index) in disSource">
+      <span class="content">{{item.text}}</span><span class="del" @click="delTag(index, false)">&times;</span>
+    </div>
+    <input class="tags-input uk-form-blank" type="text" placeholder="标签，按 enter 创建" v-model="text" @keyup.enter="addTag(text)" @keydown.delete="delTag(source.length - 1, true)">
+  </div>`
+
+  var tags = Vue.extend({
+    template,
+    props: {
+      source: {
+        type: Array,
+        default: []
+      }
+    },
+    data: function() {
+      var disSource = []
+      this.source.forEach(function (item) {
+        var obj = {
+          text: item,
+          bgc_no: Math.ceil(Math.random() * 10) - 1
+        }
+        disSource.push(obj)
+      })
+      return {
+        text: '',
+        bgc: ['#e961b4', '#ed664b', '#7b6ac7', '#56abd1', '#f7af4c', '#fe5467', '#52c7bd', '#a479b7', '#cb81ce', '#5eabc5'],
+        disSource: disSource
+      }
+    },
+    methods: {
+      pasteText(e) {
+        e.preventDefault()
+        var text = (e.clipboardData || window.clipboardData).getData('text')
+        this.addTag(text)
+      },
+      addTag(text){
+        if(text.trim() != ''){
+          var count = this.source.length
+          this.source.splice(count, 0, text)
+          this.disSource.splice(count, 0, {
+            text: text,
+            bgc_no: Math.ceil(Math.random() * 10) - 1
+          })
+          this.text = ''
+        }
+      },
+      delTag(index, way){
+        if(way){
+          if(index >=0 && this.text == ''){
+            this.source.splice(index, 1)
+            this.disSource.splice(index, 1)
+          }
+        }else {
+          this.source.splice(index, 1)
+          this.disSource.splice(index, 1)
+        }
+      }
+    }
+  })
+
+  components.tags = tags
+})(Vue, function () {
+  window.components = window.components ? window.components : {}
+  return window.components
+}())
